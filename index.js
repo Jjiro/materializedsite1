f6d76f7a7c4f8193b4a5dae938f2fcc1f1ac5cdf
Jjiro <egohappensoon@gmail.com>
2020-07-02 23:00:01 +0300

    Added sections for search, locations and social icons to the SPA...

--- index.js
+++ index.js
@@ -10,3 +10,21 @@ M.Slider.init(slider, {
   transition: 500,
   interval: 6000,
 });
+
+// Autocomplete
+const ac = document.querySelector('.autocomplete');
+M.Autocomplete.init(ac, {
+  data: {
+    Canyon: null,
+    Beaches: null,
+    Europe: null,
+    Americas: null,
+    Africa: null,
+    Asia: null,
+    Caribbean: null,
+    Miami: null,
+    Ibiza: null,
+    Dubai: null,
+    Zanzibar: null,
+  },
+});
